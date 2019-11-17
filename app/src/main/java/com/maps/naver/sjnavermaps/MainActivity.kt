4cda002ebf1b8321cd9f6690ee5dc4a04ca5a5c0
SEOUNGJIN KIM <dge03078@gmail.com>
2019-11-17 19:44:08 +0900

    make android porject

---
+++ app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maps.naver.sjnavermaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
