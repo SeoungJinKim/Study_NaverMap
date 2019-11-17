9f3b5212f58f94ebcadbf7d8409ed899a6b2e2e4
SEOUNGJIN KIM <dge03078@gmail.com>
2019-11-18 03:25:18 +0900

    success load naver map

--- app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
+++ app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
@@ -2,11 +2,18 @@ package com.maps.naver.sjnavermaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
     }
 }
