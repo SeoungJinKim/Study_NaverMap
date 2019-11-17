cb31deddd193124838dfeb29ca791f84b2244771
SEOUNGJIN KIM <dge03078@gmail.com>
2019-11-18 04:48:37 +0900

    add anko library and toast location when click map

--- app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
+++ app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
@@ -2,18 +2,45 @@ package com.maps.naver.sjnavermaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+        }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as MapFragment?
             ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
+                supportFragmentManager.beginTransaction().add(R.id.map, it).commit()
             }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        if (item.itemId == android.R.id.home) {
+            finish()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        naverMap.setOnMapClickListener { _, coord ->
+            toast(getString(R.string.format_map_click, coord.latitude, coord.longitude))
+        }
+
+        naverMap.setOnMapLongClickListener { _, coord ->
+            toast(getString(R.string.format_map_long_click, coord.latitude, coord.longitude))
+        }
     }
 }
