04ee54fd3f3335f54018dfc758d8bffa91650740
SEOUNGJIN KIM <dge03078@gmail.com>
2019-11-19 03:04:18 +0900

    refactoring code and make fab button for control markers

--- app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
+++ app/src/main/java/com/maps/naver/sjnavermaps/MainActivity.kt
@@ -2,13 +2,27 @@ package com.maps.naver.sjnavermaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.db.NULL
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var state : State = State.INITIAL
+
+    private var departureMarker : Marker?= null
+    private var arrivalMarker : Marker?= null
+
+    enum class State{
+        INITIAL, START, END, SHOW_PATH, NONE_PATH
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,7 +37,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             ?: run {
                 val options = NaverMapOptions().camera(
                     CameraPosition(
-                        NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 30.0, 45.0)
+                        NaverMap.DEFAULT_CAMERA_POSITION.target,
+                        NaverMap.DEFAULT_CAMERA_POSITION.zoom,
+                        30.0,
+                        45.0
+                    )
                 )
                 MapFragment.newInstance(options).also {
                     supportFragmentManager.beginTransaction().add(R.id.map, it).commit()
@@ -41,15 +59,49 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
     override fun onMapReady(naverMap: NaverMap) {
+
         naverMap.setOnMapClickListener { _, coord ->
             toast(getString(R.string.format_map_click, coord.latitude, coord.longitude))
         }
 
         naverMap.setOnMapLongClickListener { _, coord ->
-            toast(getString(R.string.format_map_long_click, coord.latitude, coord.longitude))
-            Marker().apply {
-                position = LatLng(coord.latitude, coord.longitude)
-                map = naverMap
+            toast(
+                getString(
+                    R.string.format_map_long_click,
+                    coord.latitude,
+                    coord.longitude
+                )
+            )
+            when (state) {
+                State.INITIAL -> {
+                    departureMarker = Marker().apply {
+                        position = LatLng(coord.latitude, coord.longitude)
+                        captionText = "출발"
+                        map = naverMap
+                    }
+                    state = State.START
+                }
+                State.START -> {
+                    arrivalMarker= Marker().apply {
+                        captionText = "도착"
+                        position = LatLng(coord.latitude, coord.longitude)
+                        map = naverMap
+                    }
+                    state = State.END
+                }
+            }
+        }
+
+        fab_remove_markers.setOnClickListener {
+            if(state != State.INITIAL)
+            {
+                if(state == State.START) departureMarker?.map = null
+                else
+                {
+                    departureMarker?.map = null
+                    arrivalMarker?.map = null
+                }
+                state = State.INITIAL
             }
         }
     }
